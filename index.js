5ca2bf165946e68b32b37a8e0bffa530731cbc02
Vastro-lorde <omatsolaseund@gmail.com>
2024-02-02 14:33:20 +0100

    tires now follow the car

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const canvas = document.getElementById('canvas');
 canvas.width = 500;
-canvas.height = window.innerHeight;
+
 
 const ctx = canvas.getContext('2d');
 const car = new Car(250, 250, 60, 100);
 car.drawCar(ctx);
 
-console.log(car);
+render()
+
+function render() {
+    car.update();
+    canvas.height = window.innerHeight;
+    car.drawCar(ctx);
+    requestAnimationFrame(render);
+}
--- src/Car.js
+++ src/Car.js
@@ -2,33 +2,94 @@ class Car {
     constructor(x, y, width, height) {
         this.x = x;
         this.y = y;
+        this.angle = 0;
         this.width = width;
         this.height = height; 
         this.speed = 0;
+        this.maxSpeed = 4;
+        this.friction = 0.05;
         this.acceleration = 0.2;
         this.frontTires = [];
         this.backTires = [];
+        this.controls = new Controls();
+    }
+
+    update(){
+        if (this.controls.forward) {
+            this.speed += this.acceleration;
+        }
+        if (this.controls.backward) {
+            this.speed -= this.acceleration;
+        }
+        if (this.speed > this.maxSpeed) {
+            this.speed = this.maxSpeed;
+        }
+        if (this.speed < -this.maxSpeed / 2) {
+            this.speed = -this.maxSpeed / 2;
+        }
+        if (this.speed > 0) {
+            this.speed -= this.friction;
+        }
+        if (this.speed < 0) {
+            this.speed += this.friction;
+        }
+        if (Math.abs(this.speed) < this.friction) {
+            this.speed = 0;
+        }
+        if (this.controls.left) {
+            for (const tire of this.frontTires) {
+                tire.turn(0.03, 'left');
+            }
+            if (this.speed != 0){
+                this.angle += 0.03;
+            }
+            console.log(this.frontTires[0].angle);
+        }
+        if (this.controls.right) {
+            for (const tire of this.frontTires) {
+                tire.turn(0.03, 'left');
+            }
+            if (this.speed != 0){
+                this.angle -= 0.03;
+            }
+        }
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
     }
 
     drawCar(ctx){
-        
-        const tire = new Tire(this.x, this.y, 20, 40);
-        tire.draw(ctx, this, 1);
-        const tire2 = new Tire(this.x, this.y, 20, 40);
-        tire2.draw(ctx, this, 2);
-        const tire3 = new Tire(this.x, this.y, 20, 40);
-        tire3.draw(ctx, this, 3);
-        const tire4 = new Tire(this.x, this.y, 20, 40);
-        tire4.draw(ctx, this, 4);
+        ctx.save();
         ctx.beginPath();
+        ctx.translate(this.x, this.y)
+        ctx.rotate(-this.angle)
         ctx.rect(
-            this.x -this.width / 2,
-            this.y -this.height / 2,
+            -this.width / 2,
+            -this.height / 2,
             this.width,
             this.height
         );
         ctx.fillStyle = 'blue';
         ctx.fill();
+
+
+        const tire = new Tire(this.x, this.y, 20, 40, this.angle, 1);
+        const tire2 = new Tire(this.x, this.y, 20, 40, this.angle, 2);
+        const tire4 = new Tire(this.x, this.y, 20, 40, this.angle, 3);
+        const tire3 = new Tire(this.x, this.y, 20, 40, this.angle, 4);
+
+        
+        tire.draw(ctx, this);
+        tire2.draw(ctx, this);
+        tire3.draw(ctx, this);
+        tire4.draw(ctx, this);
+        this.frontTires.push(tire);
+        this.frontTires.push(tire2);
+        
+        this.backTires.push(tire3);
+        this.backTires.push(tire4);
+        
+        ctx.restore()
     }
 
     
@@ -36,32 +97,40 @@ class Car {
 }
 
 class Tire {
-    constructor(x, y, width, height, offset = 10) {
+    constructor(x, y, width, height, carAngle, position, offset = 10) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.angle = carAngle;
+        this.maxAngle = 60;
         this.offset = offset;
+        this.position = position;
     }
-    draw(ctx, car, position, angle) {
+
+    
+    draw(ctx, car) {
+        ctx.beginPath();
+        ctx.translate(-this.x, -this.y)
         let a , b = 0;
-        if (position === 1) {
+        if (this.position === 1) {
             a = (car.x - car.width / 2) - this.offset;
             b = (car.y - car.height / 2) ;
         }
-        if (position === 2) {
+        if (this.position === 2) {
             a = car.x + ((car.width / 2) - this.width) + this.offset;
             b = (car.y - car.height / 2);
         }
-        if (position === 3) {
+        if (this.position === 3) {
             a = (car.x - car.width / 2) - this.offset;
             b = car.y + ((car.height / 2) - this.height);
         }
-        if (position === 4) {
+        if (this.position === 4) {
             a = car.x + ((car.width / 2) - this.width) + this.offset;
             b = car.y + ((car.height / 2) - this.height);
         }
-        ctx.beginPath();
+        
+        
         ctx.roundRect(
             a,
             b,
@@ -69,10 +138,26 @@ class Tire {
             this.height,
             30
         );
+        
         ctx.fillStyle = 'black';
         ctx.fill();
+        ctx.translate(this.x, this.y)
+
+        // if (this.position === 1 || this.position === 2) {
+        //     console.log(this.angle, this.x, this.y);
+        //     ctx.rotate(this.angle);
+        // }
+
     }
-    turn(angle){
-        
+    
+    turn(carAngle, direction){
+        if ((carAngle <= this.maxAngle) && (this.position === 1 || this.position === 2)) {
+            if (direction === "left") {
+                this.angle -= carAngle
+            }
+            if (direction === "rigth") {
+                this.angle += carAngle
+            }
+        }
     }
 }
